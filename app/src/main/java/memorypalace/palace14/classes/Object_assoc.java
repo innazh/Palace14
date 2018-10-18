88381a9ef27cd6c21ed71797f95f2b95bdf5c6f3
pliakhov <pauluniver@gmail.com>
2018-10-17 22:18:09 -0400

    Added Object Association Icons

--- app/src/main/java/memorypalace/palace14/classes/Object_assoc.java
+++ app/src/main/java/memorypalace/palace14/classes/Object_assoc.java
@@ -1,5 +1,11 @@
 package memorypalace.palace14.classes;
 
+import android.graphics.Bitmap;
+
 public class Object_assoc {
+    String o_name;
+    String o_desc;
+    String o_Symbol;
+    Bitmap o_img;
 
 }
