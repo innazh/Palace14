702d2699eacf0b2f494d15965e989b18b4d4b661
innazh <innazh1133@gmail.com>
2019-02-20 00:44:08 -0500

    Routes in progress

---
+++ app/src/main/java/memorypalace/palace14/CreateRoute.java
@@ -0,0 +1,13 @@
+package memorypalace.palace14;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateRoute extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_route);
+    }
+}
